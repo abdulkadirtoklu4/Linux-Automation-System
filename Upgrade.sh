a4ba7467e1607d4dc134e5150bfaa39ec6f529fe
Abdülkadir Toklu <91195747+abdulkadirtoklu4@users.noreply.github.com>
2023-01-23 23:07:05 +0300

    Add files via upload

---
+++ Upgrade.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Program Started;
+apt-get install upgrade -y
+apt-get install update -y
+apt-get install dsniff -y
+apt-get install bettercap -y
+apt-get install tor -y 
+apt-get install aircrack-ng -y
+echo -e "\033[32m[+]\033[0m Linux Upgraded."
+echo -e "\033[32m[+]\033[0m Installed Package For Arp Spoofing."
+echo -e "\033[32m[+]\033[0m Installed 'Bettercap'."
+echo -e "\033[32m[+]\033[0m Installed Tor Service."
+echo -e "\033[32m[+]\033[0m Installed 'Aircrack-ng'."
+# Program Finished.
