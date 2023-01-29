012dc27aaa0da67ea2dc98fe91810a219ab35019
abdulkadirtoklu4 <abdulkadir74280@gmail.com>
2023-01-30 02:05:05 +0300

    "clear" append.

--- Upgrade.sh
+++ Upgrade.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 # Program Started;
+#if [ "`id -u`" -eq 0 ]; then
+#	complain "Do Not Run This Program As a Root User."
+#	exit 1
+#fi
 apt-get install upgrade -y
 apt-get install update -y
 apt-get install dsniff -y
 apt-get install bettercap -y
 apt-get install tor -y 
 apt-get install aircrack-ng -y
+clear
 echo -e "\033[32m[+]\033[0m Linux Upgraded."
 echo -e "\033[32m[+]\033[0m Installed Package For Arp Spoofing."
 echo -e "\033[32m[+]\033[0m Installed 'Bettercap'."
