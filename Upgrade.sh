1754a9a440e45a7a74ceb02b2f3e3d3d42355869
abdulkadirtoklu4 <abdulkadir74280@gmail.com>
2023-02-04 23:26:37 +0300

    Some programs have been added.

--- Upgrade.sh
+++ Upgrade.sh
@@ -9,12 +9,18 @@ apt-get install upgrade -y
 apt-get install update -y
 apt-get install dsniff -y
 apt-get install bettercap -y
-apt-get install tor -y 
+apt-get install tor -y
 apt-get install aircrack-ng -y
+apt-get install powercat -y
+apt-get install maltego -y
+apt-get install wine64 -y
 clear
 echo -e "\033[32m[+]\033[0m Linux Upgraded."
 echo -e "\033[32m[+]\033[0m Installed Package For Arp Spoofing."
 echo -e "\033[32m[+]\033[0m Installed 'Bettercap'."
 echo -e "\033[32m[+]\033[0m Installed Tor Service."
 echo -e "\033[32m[+]\033[0m Installed 'Aircrack-ng'."
+echo -e "\033[32m[+]\033[0m Installed 'Powercat'."
+echo -e "\033[32m[+]\033[0m Installed 'Maltego'."
+echo -e "\033[32m[+]\033[0m Installed 'Wine64'."
 # Program Finished.
