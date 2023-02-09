8a62114ec2aa8d00933f5ad46965bf46ab9bff03
abdulkadirtoklu4 <abdulkadir74280@gmail.com>
2023-02-10 01:38:21 +0300

    "Shodan", "Maltego" Added.

--- Upgrade.sh
+++ Upgrade.sh
@@ -14,6 +14,11 @@ apt-get install aircrack-ng -y
 apt-get install powercat -y
 apt-get install maltego -y
 apt-get install wine64 -y
+apt-get install beef -y
+apt-get install alien -y
+apt-get install smtp-user-enum -y
+pip install shodan
+apt-get install maltegoce -y
 clear
 echo -e "\033[32m[+]\033[0m Linux Upgraded."
 echo -e "\033[32m[+]\033[0m Installed Package For Arp Spoofing."
@@ -23,4 +28,9 @@ echo -e "\033[32m[+]\033[0m Installed 'Aircrack-ng'."
 echo -e "\033[32m[+]\033[0m Installed 'Powercat'."
 echo -e "\033[32m[+]\033[0m Installed 'Maltego'."
 echo -e "\033[32m[+]\033[0m Installed 'Wine64'."
+echo -e "\033[32m[+]\033[0m Installed 'Beef'."
+echo -e "\033[32m[+]\033[0m Installed 'Alien'."
+echo -e "\033[32m[+]\033[0m Installed SMTP Service."
+echo -e "\033[32m[+]\033[0m Installed 'Shodan'."
+echo -e "\033[32m[+]\033[0m Installed 'Maltego'."
 # Program Finished.
